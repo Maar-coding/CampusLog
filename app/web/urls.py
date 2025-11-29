224b3354667f219646c8d1986fda7f6670cd2fee
Yeseo Ku <kuyeseo1206@gmail.com>
2025-11-29 16:48:59 +0900

    [fix]게시글 리스트, 게시글 작성 후 반영 수정

--- app/web/urls.py
+++ app/web/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('test', views.test_page, name='test'),
     path('start', views.start_page, name='start'),
-    path('board',views.board,name='board'),
+    path('board', views.board_list, name='board'),
     path('register', views.register, name='register'),
     # 2. 식당 및 리뷰 (명세서 기반)
     # {id} -> <int:id>
